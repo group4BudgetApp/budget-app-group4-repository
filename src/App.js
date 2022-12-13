8319a14c469aaa2cf062c421df11c3f051b8236a
olstinker <giazozula@gmail.com>
2022-12-13 14:45:29 -0500

    inital JSX structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import "./App.css";
 import firebase from "./firebase";
-import {getDatabase, ref} from "firebase/database";
+import {useState, useEffect} from 'react';
+import {getDatabase, ref, onValue, push, remove} from "firebase/database";
+import {Route, Routes} from 'react-router-dom';
+import Arrow from "./Arrow";
+import Logo from "./Logo"
+import NavBar from "./NavBar";
+import LiveBudget from './LiveBudget';
 
-// Firebase initialization
-const database = getDatabase(firebase);
-// dbRed will reference our database
-const dbRef = ref(database);
-// Test firebase
-console.log(dbRef);
 
 function App() {
-	return <div className="App">
-    <p>test</p>
-    <p>gia-test</p>
-  </div>;
+
+  useEffect(() => {
+    // Firebase initialization
+    const database = getDatabase(firebase);
+    // dbRed will reference our database
+    const dbRef = ref(database);
+    // Test firebase
+    console.log(dbRef);
+  }, []) // End of useEffect
+
+
+  // JSX
+	return ( 
+  <div className="App">
+    <div className="wrapper">
+    <header>
+      {/* Logo Component */}
+      <Logo />
+      {/* Nav Bar Component */}
+      <NavBar />
+    </header>
+    <main>
+      <section className="budgetForm">
+        {/* budgetForm Component */}
+      </section>
+      <section className="arrowButton">
+        <Arrow />
+      </section>
+      <section className="expensesForm">
+        {/* expensesForm Component */}
+      </section>
+    </main>
+    <footer>
+      {/* Footer Component */}
+    </footer>
+    </div> {/* End of Wrapper */}
+  </div>; // End of App
+
+// Routes
+<Routes>
+</Routes>       
+  )
 }
 
 export default App;
