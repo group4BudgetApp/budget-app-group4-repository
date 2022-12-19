5f2af050205d871a8e8c2201db0cb2457c00808d
YanChen <yc.yanchen@hotmail.com>
2022-12-19 16:21:32 -0500

    made changes to firebase settings

--- src/firebase.js
+++ src/firebase.js
@@ -5,7 +5,6 @@ import {initializeApp} from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-<<<<<<< HEAD
 	apiKey: "AIzaSyB_msuY16IMAzdem3VWUoKnhuYRhZUOHa4",
 	authDomain: "budgetapp-f5759.firebaseapp.com",
 	databaseURL: "https://budgetapp-f5759-default-rtdb.firebaseio.com",
@@ -13,17 +12,7 @@ const firebaseConfig = {
 	storageBucket: "budgetapp-f5759.appspot.com",
 	messagingSenderId: "699291830100",
 	appId: "1:699291830100:web:0911e2369d116e95376ac1",
-=======
-	apiKey: "AIzaSyACVh8W22tUAXIlEtf2hknX1Ln_KKynKds",
-	authDomain: "learning-firebase-2a5a8.firebaseapp.com",
-	databaseURL: "https://learning-firebase-2a5a8-default-rtdb.firebaseio.com",
-	projectId: "learning-firebase-2a5a8",
-	storageBucket: "learning-firebase-2a5a8.appspot.com",
-	messagingSenderId: "392889442435",
-	appId: "1:392889442435:web:47827be5354c2cc3e33bb6",
->>>>>>> a9074d224fa8d51ece0c8fc7b4d5e11e50024856
 };
-
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
 
