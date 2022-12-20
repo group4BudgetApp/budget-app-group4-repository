65d4b413ed2444f43453b706cb79ec163acd1781
HV <heather.m.voisin@gmail.com>
2022-12-20 15:17:03 -0500

    added item delete button

--- src/SpendingForm.js
+++ src/SpendingForm.js
@@ -1,49 +1,50 @@
 import {push, set} from "firebase/database";
 import {useState} from "react";
-
+ 
 const SpendingForm = ({dbSpending, daysSince, userBalance, setUserBalance, dbBalance, daysUntil}) => {
-	// Form input onChange for the spending data will be stored in this state
-	const [newSpendingData, setNewSpendingData] = useState({});
-	// Tracks changes in the SpendingForm
-	const spendingOnChange = (e) => {
-		// Store the changes inside a variable
-		const tempVal = e.target.value;
-		setNewSpendingData({
-			// Spreading whatever existing data is in newSpendingData
-			...newSpendingData,
-			// Target the name attribute and use that as the property and pair it with the tempVal. Overwrites an existing value if the name matches to 'update' the value
-			[e.target.name]: tempVal,
-		});
-	};
-
-	// Handles the submission behavior of the SpendingForm
-	const spendingOnSubmit = (e) => {
-		e.preventDefault();
-		// Subtracts the userBalance by the expense cost
-		const tempCalc = userBalance - newSpendingData.expenseCost;
-		// Update the state with tempCalc
-		setUserBalance(tempCalc);
-		// Set tempCalc into firebase. Set because it will overwrite the previous entry (Updating the balance)
-		set(dbBalance, tempCalc);
-		// Push the key value pair of the expense to the spending node. Push to get a new firebase key.
-		push(dbSpending, {[newSpendingData.expenseName]: parseInt(newSpendingData.expenseCost)});
-		// Resets the form
-		e.target.reset();
-	};
-
-	return (
-		<>
-			<form onSubmit={spendingOnSubmit} className="spendingFormContainer">
-				<h2 className="dayCount">Day: {daysSince}</h2>
-				<h2 className="dayCount">Days until next pay: {daysUntil}</h2>
-				<input type="text" id="expenseName" name="expenseName" placeholder="Expense Name" onChange={spendingOnChange} />
-				<input type="number" id="expenseCost" name="expenseCost" placeholder="Expense Cost" onChange={spendingOnChange} />
-				<button type="submit" className="rectangleButton">
-					Submit Expense
-				</button>
-			</form>
-		</>
-	);
+   // Form input onChange for the spending data will be stored in this state
+   const [newSpendingData, setNewSpendingData] = useState({});
+   // Tracks changes in the SpendingForm
+   const spendingOnChange = (e) => {
+       // Store the changes inside a variable
+       const tempVal = e.target.value;
+       setNewSpendingData({
+           // Spreading whatever existing data is in newSpendingData
+           ...newSpendingData,
+           // Target the name attribute and use that as the property and pair it with the tempVal. Overwrites an existing value if the name matches to 'update' the value
+           [e.target.name]: tempVal,
+       });
+       console.log(newSpendingData);
+   };
+ 
+   // Handles the submission behavior of the SpendingForm
+   const spendingOnSubmit = (e) => {
+       e.preventDefault();
+       // Subtracts the userBalance by the expense cost
+       const tempCalc = userBalance - newSpendingData.expenseCost;
+       // Update the state with tempCalc
+       setUserBalance(tempCalc);
+       // Set tempCalc into firebase. Set because it will overwrite the previous entry (Updating the balance)
+       set(dbBalance, tempCalc);
+       // Push the key value pair of the expense to the spending node. Push to get a new firebase key.
+       push(dbSpending, {[newSpendingData.expenseName]: parseInt(newSpendingData.expenseCost)});
+       // Resets the form
+       e.target.reset();
+   };
+ 
+   return (
+       <>
+           <form onSubmit={spendingOnSubmit} className="spendingFormContainer">
+               <h2 className="dayCount">Day: {daysSince}</h2>
+               <h2 className="dayCount">Days until next pay: {daysUntil}</h2>
+               <input type="text" id="expenseName" name="expenseName" placeholder="Expense Name" onChange={spendingOnChange} />
+               <input type="number" id="expenseCost" name="expenseCost" placeholder="Expense Cost" onChange={spendingOnChange} />
+               <button type="submit" className="rectangleButton">
+                   Submit Expense
+               </button>
+           </form>
+       </>
+   );
 };
-
+ 
 export default SpendingForm;
--- src/SpendingInterface.js
+++ src/SpendingInterface.js
@@ -1,29 +1,28 @@
 import SpendingForm from "./SpendingForm";
 import SpendingDisplay from "./SpendingDisplay";
 import Balance from "./Balance";
-import {Navigate} from "react-router-dom";
-
-const SpendingInterface = ({getSpendingData, dbSpending, daysSince, userSpendingData, userBalance, setUserBalance, dbBalance, daysUntil}) => {
-	return (
-		<>
-			<section className="spendingContainer">
-				<Balance userBalance={userBalance} daysUntil={daysUntil} />
-				<div className="interactiveContainer">
-					<SpendingForm
-						getSpendingData={getSpendingData}
-						dbSpending={dbSpending}
-						daysSince={daysSince}
-						setUserBalance={setUserBalance}
-						dbBalance={dbBalance}
-						userBalance={userBalance}
-						daysUntil={daysUntil}
-					/>
-					<SpendingDisplay daysSince={daysSince} userSpendingData={userSpendingData} userBalance={userBalance} />
-					{!userBalance ? <Navigate to="/" /> : null}
-				</div>
-			</section>
-		</>
-	);
+ 
+const SpendingInterface = ({getSpendingData, dbSpending, daysSince, userSpendingData, userBalance, setUserBalance, dbBalance, daysUntil, userID}) => {
+   return (
+       <>
+           <section className="spendingContainer">
+               <Balance userBalance={userBalance} daysUntil={daysUntil} />
+               <div className="interactiveContainer">
+                   <SpendingForm
+                       getSpendingData={getSpendingData}
+                       dbSpending={dbSpending}
+                       daysSince={daysSince}
+                       setUserBalance={setUserBalance}
+                       dbBalance={dbBalance}
+                       userBalance={userBalance}
+                       daysUntil={daysUntil}
+                   />
+                   <SpendingDisplay daysSince={daysSince} userSpendingData={userSpendingData} userBalance={userBalance} userID={userID} setUserBalance={setUserBalance}/>
+               </div>
+           </section>
+       </>
+   );
 };
-
+ 
 export default SpendingInterface;
+
